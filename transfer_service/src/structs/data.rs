d3c1fb55a4472dca985af53ad2879e22634ba5f2
MwlLj <731025894@qq.com>
2019-07-19 20:36:22 +0800

    移除 data.rs

--- transfer_service/src/structs/data.rs
+++
@@ -1,8 +0,0 @@
-#[derive(Default)]
-pub struct CData {
-    pub selfCommunicateUuid: String,
-    pub peerCommunicateUuid: String,
-    pub packageIndex: u64,
-    pub packageTotal: u64,
-    pub data: Vec<u8>
-}
--- transfer_service/src/structs/response.rs
+++ transfer_service/src/structs/response.rs
@@ -1,5 +1,11 @@
 #[derive(Default)]
 pub struct CResponse {
+    pub responseMode: String,
     pub serverUuid: String,
-    pub result: i32
+    pub result: u8,
+    pub selfCommunicateUuid: String,
+    pub peerCommunicateUuid: String,
+    pub packageIndex: u64,
+    pub packageTotal: u64,
+    pub data: Vec<u8>
 }
